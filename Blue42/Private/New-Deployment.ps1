69c096b34b2a769221aa925baaa79b3a8ad27d11
Ignacio Galarza <igalarza@nephilaadvisors.com>
2018-09-17 20:54:48 -0500

    Added Deployment and consolidadted tests

---
+++ Blue42/Private/New-Deployment.ps1
@@ -0,0 +1,72 @@
+function New-Deployment {
+    [CmdletBinding()]
+    <#
+        .SYNOPSIS
+        Retrieves service pack and operating system information from one or more remote computers.
+        .DESCRIPTION
+        The New-Deployment function uses the AzureRM module to perform a New-AzureRmResourceGroupDeployment into a Resource Group.
+        If the Resource Group doesn't exist, the function will create it before performing the deployment.
+        .NOTES
+        You need to run this function after establishing an AzureRm context using Login-AzureRmAccount.
+    #>    
+    param (
+        # The destination Resource Group Name
+        [Parameter(Mandatory=$true)]
+        [string] $ResourceGroupName,
+
+        # The destination Azure region
+        [Parameter(Mandatory=$true)]
+        [string] $Location,
+
+        # The full path to the source ARM template
+        [Parameter(Mandatory=$true)]
+        [string] $TemplatePath,
+
+        # The collection of template parameters
+        [Parameter(Mandatory=$true)]
+        [hashtable] $TemplateParams,
+
+        # The name of the Azure Deployment
+        [Parameter(Mandatory=$false)]
+        [string] $Name = ("B42{0}" -f (Get-DateTime15)),
+
+        # The deployment mode
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('Incremental', 'Complete')]
+        [string] $Mode = "Incremental",
+
+        # The debug level for the deployment
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('RequestContent', 'ResponseContent', 'All', 'None')]
+        [string] $DeploymentDebugLogLevel = "None"
+    )
+
+    begin {
+        Write-Verbose "Starting New-Deployment"
+    }
+
+    process {
+        $rg = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction 0
+        if ($null -eq $rg) {
+            $rg = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+        }
+
+        $deploymentParameters = @{
+            ResourceGroupName       = $ResourceGroupName
+            TemplateFile            = $TemplatePath
+            TemplateParameterObject = $TemplateParams
+            Name                    = $Name
+            Mode                    = $Mode
+            DeploymentDebugLogLevel = $DeploymentDebugLogLevel
+        }
+        $deploymentResults = New-AzureRmResourceGroupDeployment @deploymentParameters -Confirm:$false -Force
+        if ($deploymentResults.ProvisioningState -eq "Failed") {
+            Write-Error ("The {0} deployment failed." -f $Name)
+        }
+        $deploymentResults
+    }
+
+    end {
+        Write-Verbose "Ending New-Deployment"
+    }
+}
--- tests/unit/Blue42.Tests.ps1
+++ tests/unit/Blue42.Tests.ps1
@@ -4,34 +4,79 @@ $SuppressImportModule = $false
 . "$PSScriptRoot\..\LoadModule.ps1"
 
 Describe "the basic module" {
-    InModuleScope $ModuleName {
-        It "generates a unique date identifier" {
-            $dateUID = Get-DateTime15
-            $dateUID.Length | Should Be (15)
-            $dateUID.GetType() | Should Be ("string")
+    Context "Private functions with Azure mocks" {
+        Mock -ModuleName $ModuleName Get-AzureRmResourceGroup { return $null }
+        Mock -ModuleName $ModuleName New-AzureRmResourceGroup { return $null }
+        Mock -ModuleName $ModuleName New-AzureRmResourceGroupDeployment {
+            $mockDeploymentResult = [ordered]@{
+                DeploymentName          = $Name
+                ResourceGroupName       = $ResourceGroupName
+                ProvisioningState       = "Succeeded"
+                Timestamp               = "NOTSTAMPED"
+                Mode                    = $Mode
+                TemplateLink            = ""
+                Parameters              = $TemplateParameterObject
+                Outputs                 = ""
+                DeploymentDebugLogLevel = $DeploymentDebugLogLevel
+            }
+            return $mockDeploymentResult
         }
 
-        It "parses the module test template" {
-            $template = Get-Template -TemplatePath "$PSScriptRoot\input\Blue42.Test.json" -SkipTokenReplacement $true
-            ($template.'$schema' -eq "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#") | Should Be ($true)
-            ($template.parameters["Blue42Password"].defaultValue -eq "get[PASSWORD]") | Should Be ($true)
-            ($template.parameters["Blue42UID"].defaultValue -eq "root[UID]") | Should Be ($true)
-            ($template.parameters["Blue42Location"].defaultValue -eq "azure[LOCATION]") | Should Be ($true)
+        InModuleScope $ModuleName {
+            It "generates a unique date identifier" {
+                $dateUID = Get-DateTime15
+                $dateUID.Length | Should Be (15)
+                $dateUID.GetType() | Should Be ("string")
+            }
+
+            It "parses the module test template" {
+                $template = Get-Template -TemplatePath "$PSScriptRoot\input\Blue42.Test.json" -SkipTokenReplacement $true
+                ($template.'$schema' -eq "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#") | Should Be ($true)
+                ($template.parameters["Blue42Password"].defaultValue -eq "get[PASSWORD]") | Should Be ($true)
+                ($template.parameters["Blue42UID"].defaultValue -eq "root[UID]") | Should Be ($true)
+                ($template.parameters["Blue42Location"].defaultValue -eq "azure[LOCATION]") | Should Be ($true)
+            }
+
+            It "performs a simple deployment" {
+                $mockParameters = @{
+                    Blue42Password = "get[PASSWORD]"
+                    Blue42UID      = "root[UID]"
+                    Blue42Location = "azure[LOCATION]"
+                }
+
+                $deploymentResult = New-Deployment -Location "blueMan" -ResourceGroupName "deploymenttest-rg" -TemplatePath "$PSScriptRoot\input\Blue42.Test.json" -TemplateParams $mockParameters
+                ($deploymentResult.Parameters.Count) | Should Be (3)
+
+                $deploymentResult.Parameters.Contains("Blue42Password") | Should Be ($true)
+                $deploymentResult.Parameters.Contains("Blue42Location") | Should Be ($true)
+                $deploymentResult.Parameters.Contains("Blue42UID") | Should Be ($true)
+            }
         }
     }
 
-    It "passes basic globals workflow" {
-        $globals = Get-B42Globals
-        ($globals.UID.Length) | Should Be (15)
-        ([string]::IsNullOrEmpty($globals.Location)) | Should Be ($false)
-        (Test-Path $globals.TemplatePath) | Should Be ($true)
-        # The Sleep statement is required to make sure that that the values for UID change.
-        Start-Sleep -Seconds 1
-        $null = Set-B42Globals
-        $newGlobals = Get-B42Globals
-        ($globals.Location -eq $newGlobals.Location) | Should Be ($true)
-        ($globals.TemplatePath -eq $newGlobals.TemplatePath) | Should Be ($true)
-        ($globals.UID -eq $newGlobals.UID) | Should Be ($false)
+    Context "Helper functions" {
+        It "passes basic globals workflow" {
+            $globals = Get-B42Globals
+            ($globals.UID.Length) | Should Be (15)
+            ([string]::IsNullOrEmpty($globals.Location)) | Should Be ($false)
+            (Test-Path $globals.TemplatePath) | Should Be ($true)
+            # The Sleep statement is required to make sure that that the values for UID change.
+            Start-Sleep -Seconds 1
+            $null = Set-B42Globals
+            $newGlobals = Get-B42Globals
+            ($globals.Location -eq $newGlobals.Location) | Should Be ($true)
+            ($globals.TemplatePath -eq $newGlobals.TemplatePath) | Should Be ($true)
+            ($globals.UID -eq $newGlobals.UID) | Should Be ($false)
+        }
+
+        It "generates a password with the minimum security requirements." {
+            $password = New-B42Password
+            ($password -match ("\W")) | Should Be ($true)
+            ($password -match ("\d")) | Should Be ($true)
+            ($password -match ("[A-Z]")) | Should Be ($true)
+            ($password -match ("[a-z]")) | Should Be ($true)
+            ($password.Length -gt 36) | Should Be ($true)
+        }        
     }
 
     AfterAll {
