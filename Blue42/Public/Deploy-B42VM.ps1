bb55cf64be686d108635bb66a6b75dcf8ccb3a66
Ignacio Galarza <igalarza@nephilaadvisors.com>
2018-11-04 16:45:57 -0600

    Finished VM Deployment

--- Blue42/Public/Deploy-B42VM.ps1
+++ Blue42/Public/Deploy-B42VM.ps1
@@ -23,9 +23,13 @@ function Deploy-B42VM {
         [Parameter(Mandatory = $false)]
         [System.Collections.Specialized.OrderedDictionary] $VMParameters = [ordered]@{},
 
-        # An array of script extensions parameters blocks; one per desired extension.
+        # If true, a Public IP and NSG will be created for the VM
         [Parameter(Mandatory = $false)]
-        [System.Collections.Specialized.OrderedDictionary[]] $ScriptExtensions = @()
+        [switch] $IncludePublicInterface,
+
+        # If true, the vm will use linux specific configuraiton settings. If false, the vm will use windows specific configuration settings.
+        [Parameter (Mandatory = $false)]
+        [switch] $IsLinux
     )
 
     begin {
@@ -33,17 +37,69 @@ function Deploy-B42VM {
     }
 
     process {
-        $templates = @("WinVM")
-        $deploymentResult = New-B42Deployment -ResourceGroupName $ResourceGroupName -Templates $templates -Location "$Location"
+        # The parameters in VirtualNetworkParameters are required. If not provided, create some defaults.
+        if (!($VMParameters.Contains("vnetResourceGroupName") -and $VMParameters.Contains("vnetName") -and $VMParameters.Contains("subnetName"))) {
+            $vnetParams = Get-B42TemplateParameters -Templates @("VNet")
+            $subnetParams = Get-B42TemplateParameters -Templates @("Subnet")
+            Deploy-B42VNet -ResourceGroupName $ResourceGroupName -Location $Location -VNetParameters $vnetParams -Subnets @($subnetParams)
+            # Carry along these values to the VMDeployment.
+            $VMParameters.Add("vnetResourceGroupName", $ResourceGroupName)
+            $VMParameters.Add("vnetName", $vnetParams.vnetName)
+            $VMParameters.Add("subnetName", $subnetParams.subnetName)
+        }
+
+        if (!($VMParameters.Contains("keyVaultResourceGroupName") -and $VMParameters.Contains("keyVaultName"))) {
+            $keyVaultResult = New-B42Deployment -ResourceGroupName $ResourceGroupName -Location "$Location" -Templates @("KeyVault")
+            # Carry along these values to the VMDeployment.
+            $VMParameters.Add("keyVaultResourceGroupName", $ResourceGroupName)
+            $VMParameters.Add("keyVaultName", $keyVaultResult.Parameters.keyVaultName)
+
+            # TODO Linux.
+            $certForms = Get-B42CertificateForms
+            $null = Add-Secret -KeyVaultName $keyVaultResult.Parameters.keyVaultName.Value -SecretName "CertPassword" -SecretValue $certForms.Password
+            $certSecret = Add-Secret -KeyVaultName $keyVaultResult.Parameters.keyVaultName.Value -SecretName "Cert" -SecretValue $certForms.JsonArray
+            $VMParameters.Add("vmCertificateSecretUrl", $certSecret.Id)
+
+            # Add the admin user and password.
+            # Should the VM just reference the password in the keyVault?
+            $aUser = "azdam"
+            $aPass = (New-B42Password)
+            $VMParameters.Add("vmAdminUsername", $aUser)
+            $VMParameters.Add("vmAdminPassword", $aPass)
+            Add-Secret -KeyVaultName $keyVaultResult.Parameters.keyVaultName.Value -SecretName "AdminUsername" -SecretValue $aUser
+            Add-Secret -KeyVaultName $keyVaultResult.Parameters.keyVaultName.Value -SecretName "AdminPassword" -SecretValue $aPass
+        }
+        # Create a NIC here
+        $templates = @("NetworkInterface")
+        $nicParams = Get-B42TemplateParameters -Templates $templates -TemplateParameters $VMParameters
+        if ($IncludePublicInterface) {
+            # Ok.  This should probably be more simple. Not sure that the $pipnsgResult order is guaranteed. The alternative
+            # may be simply to Test-B42Deployment to combine the parameter sets?
+            $pipnsgParameters = Get-B42TemplateParameters -Templates @("PublicIP", "NSG") -TemplateParameters $nicParams
+            $null = New-B42Deployment -ResourceGroupName $ResourceGroupName -Location "$Location" -Templates @("PublicIP", "NSG") -TemplateParameters $pipnsgParameters
+            $nicParams.publicIPName = $pipnsgParameters.publicIPName
+            $nicParams.nsgName = $pipnsgParameters.nsgName
+        }
+        $nicDeploymentResult = New-B42Deployment -ResourceGroupName $ResourceGroupName -Location "$Location" -Templates $templates -TemplateParameters $nicParams
+        $VMParameters.Add("networkInterfaceName", $nicDeploymentResult.Parameters.networkInterfaceName.Value)
+
+        # TODO: More tokens for resourceid
+        if ($VMParameters.Contains("imageName")) {
+            $VMParameters.Add("vmImagePublisher", "")
+            $VMParameters.Add("vmImageOffer", "")
+            $VMParameters.Add("vmImageSKU", "")
+        }
+
+        $deploymentResult = New-B42Deployment -ResourceGroupName $ResourceGroupName -Location "$Location" -Templates @("WinVM") -TemplateParameters $VMParameters
         $vmName = $deploymentResult.Parameters.vmName.Value
         if ([string]::IsNullOrEmpty($vmName)) {throw "Failed to obtain VM name"}
 
-        foreach ($scriptExtension in $ScriptExtensions) {
-            if (!$scriptExtension.Contains("vmName")) {
-                $scriptExtension.Add("vmName", $vmName)
+        if($deploymentResult.Parameters.vmIdentity.Value -eq "SystemAssigned"){
+            # Find the Managed Service Identity PrincipalId and grant it permission to query the secrets.
+            $vmInfoPS = Get-AzureRMVM -ResourceGroupName $ResourceGroupName -Name $vmName
+            if (![string]::IsNullOrEmpty($vmInfoPS.Identity.PrincipalId)) {
+                Set-AzureRmKeyVaultAccessPolicy -VaultName $VMParameters.keyVaultName -PermissionsToSecrets get, list -ObjectId $vmInfoPS.Identity.PrincipalId
             }
-            Write-Verbose "Etension"
-            $deploymentResult = New-B42Deployment -ResourceGroupName $ResourceGroupName -Templates @("VMExtension") -Location "$Location"
         }
     }
 
